2b776ce8217343df5cf30b640dcfe3073cb61e10
Danilo Chang <ray2501@gmail.com>
2024-11-09 19:55:38 +0800

    Update

--- generic/tclopencc.c
+++ generic/tclopencc.c
@@ -181,7 +181,7 @@ static int OpenccMain(void *cd, Tcl_Interp *interp, int objc,Tcl_Obj *const*objv
 int
 Opencc_Init(Tcl_Interp *interp)
 {
-    if (Tcl_InitStubs(interp, "8.4", 0) == NULL) {
+    if (Tcl_InitStubs(interp, TCL_VERSION, 0) == NULL) {
 	return TCL_ERROR;
     }
     if (Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION) != TCL_OK) {
