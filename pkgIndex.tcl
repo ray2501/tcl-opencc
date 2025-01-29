c2fc691274e2fd51acc66a1ec6d518c7d992cce0
Danilo Chang <ray2501@gmail.com>
2025-01-29 19:45:05 +0800

    Bump version to 0.4

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded opencc 0.3 \
-	    [list load [file join $dir libtcl9opencc0.3.so] [string totitle opencc]]
+    package ifneeded opencc 0.4 \
+	    [list load [file join $dir libtcl9opencc0.4.so] [string totitle opencc]]
 } else {
-    package ifneeded opencc 0.3 \
-	    [list load [file join $dir libopencc0.3.so] [string totitle opencc]]
+    package ifneeded opencc 0.4 \
+	    [list load [file join $dir libopencc0.4.so] [string totitle opencc]]
 }
--- tcl-opencc.spec
+++ tcl-opencc.spec
@@ -5,7 +5,7 @@
 
 Name:          tcl-opencc
 Summary:       Tcl bindings for OpenCC (libopencc)
-Version:       0.3
+Version:       0.4
 Release:       0
 License:       Apache-2.0
 Group:         Development/Libraries/Tcl
